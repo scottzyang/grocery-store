78056fad6a5475080e6ce8027e07138d985b8f38
scottzyang <scottzyang@gmail.com>
2023-02-01 17:09:08 -0600

    Complete forms

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SelectField, SubmitField
+from wtforms import StringField, DateField, SelectField, SubmitField, FloatField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Length, URL
+from grocery_app.models import *
 
 class GroceryStoreForm(FlaskForm):
     """Form for adding/updating a GroceryStore."""
@@ -10,7 +11,17 @@ class GroceryStoreForm(FlaskForm):
     # - title - StringField
     # - address - StringField
     # - submit button
-    pass
+    title = StringField('Store name', 
+        validators=[
+            DataRequired(),
+            Length(min=3, max=80, message="Your store must be between 3 and 80 characters.")
+        ])
+    address = StringField('Location', 
+        validators=[
+            DataRequired(),
+            Length(min=3, max=80, message="Your address must be between 3 and 80 characters.")
+        ])
+    submit = SubmitField('Submit')
 
 class GroceryItemForm(FlaskForm):
     """Form for adding/updating a GroceryItem."""
@@ -22,4 +33,13 @@ class GroceryItemForm(FlaskForm):
     # - photo_url - StringField
     # - store - QuerySelectField (specify the `query_factory` param)
     # - submit button
-    pass
+    name = StringField('Item name', validators=[
+        DataRequired(),
+        Length(min=3, max=80, message="Item must be between 3 and 80 characters.")
+    ])
+    price = FloatField('Item price', validators=[DataRequired()])
+    category = SelectField('Category', choices=ItemCategory.choices())
+    photo_url = StringField('Url', validators=[DataRequired()])
+    store = QuerySelectField('Store', 
+        query_factory=lambda: GroceryStore.query, allow_blank=False)
+    submit = SubmitField('Submit')
