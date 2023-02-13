baaf87b5c4ec0ae2368135ce8cf9928ab93d1458
scottzyang <scottzyang@gmail.com>
2023-02-13 14:38:32 -0600

    Move routes and forms to main directory

---
+++ grocery_app/main/routes.py
@@ -0,0 +1,99 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_required
+from datetime import date, datetime
+from grocery_app.extensions import bcrypt
+from grocery_app.models import GroceryStore, GroceryItem
+from grocery_app.main.forms import GroceryStoreForm, GroceryItemForm
+
+from grocery_app.extensions import app, db
+
+main = Blueprint("main", __name__)
+
+##########################################
+#           Routes                       #
+##########################################
+
+@main.route('/')
+def homepage():
+    all_stores = GroceryStore.query.all()
+    print(all_stores)
+    return render_template('home.html', all_stores=all_stores)
+
+@main.route('/new_store', methods=['GET', 'POST'])
+@login_required
+def new_store():
+    form = GroceryStoreForm()
+
+    if form.validate_on_submit():
+        new_store = GroceryStore(
+            title=form.title.data,
+            address=form.address.data
+        )
+        db.session.add(new_store)
+        db.session.commit()
+
+        flash('New store added!')
+        return redirect(url_for('main.store_detail', store_id=new_store.id))
+
+    return render_template('new_store.html', form=form)
+
+@main.route('/new_item', methods=['GET', 'POST'])
+@login_required
+def new_item():
+    form = GroceryItemForm()
+
+    if form.validate_on_submit():
+        new_item = GroceryItem(
+            name=form.name.data,
+            price=form.price.data,
+            category=form.category.data,
+            photo_url=form.photo_url.data,
+            store=form.store.data,
+        )
+        db.session.add(new_item)
+        db.session.commit()
+        
+        flash('Item added successfully!')
+        return redirect(url_for('main.item_detail', item_id=new_item.id))
+
+    return render_template('new_item.html', form=form)
+
+@main.route('/store/<store_id>', methods=['GET', 'POST'])
+@login_required
+def store_detail(store_id):
+    store = GroceryStore.query.get(store_id)
+    form = GroceryStoreForm(obj=store)
+
+    if form.validate_on_submit():
+        store.title = form.title.data
+        store.address = form.address.data
+
+        db.session.add(store)
+        db.session.commit()
+
+        flash('Store updated sucessfully!')
+        return redirect(url_for('main.store_detail', store_id=store.id))
+
+    store = GroceryStore.query.get(store_id)
+    return render_template('store_detail.html', store=store, form=form)
+
+@main.route('/item/<item_id>', methods=['GET', 'POST'])
+@login_required
+def item_detail(item_id):
+    item = GroceryItem.query.get(item_id)
+    form = GroceryItemForm(obj=item)
+
+    if form.validate_on_submit():
+        item.name=form.name.data,
+        item.price=form.price.data,
+        item.category=form.category.data,
+        item.photo_url=form.photo_url.data,
+        item.store=form.store.data
+
+        db.session.add(item)
+        db.session.commit()
+
+        return redirect(url_for('main.item_detail', item_id=item.id))
+
+    item = GroceryItem.query.get(item_id)
+    return render_template('item_detail.html', item=item, form=form)
